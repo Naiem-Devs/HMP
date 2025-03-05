126ced46254949ceec16793e026a3666d6230e5b
Naiem-Devs <dnsnaiem@gmail.com>
2025-03-06 01:14:19 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+(function($) {
+  "use strict";
+  
+ // menu 
+ $('.siteBar-btn').click( function (event){ 
+    event.preventDefault()
+    $(this).toggleClass('active');   
+    $('.mobile-menu').toggleClass('siteBar');   
+  }); 
+
+
+
+  // page Animation
+  AOS.init({
+    mirror: true,
+    duration: 1500,
+    initClassName: 'aos-init',
+    once: false,
+  });
+
+  // data-aos="fade-up" 
+  // data-aos-delay="300" 
+
+ 
+})(jQuery);
